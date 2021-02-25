56902cb0a0f6c56f7ee5b5a72c48d8f3a3231c8b
Riley Fournier <rileyjfournier@gmail.com>
2021-02-25 00:21:07 +0000

    hover now utilizing CSS instead of event handlers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,36 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1614011845579
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  let allTweets = ''; 
+  tweets.forEach(tweet => allTweets += createTweetElement(tweet));
+  return allTweets;
+}
 
 // helper function for returning tweet creation time
 const timeCreated = function(time) {
@@ -35,17 +54,17 @@ const createTweetElement = function(data) {
   let tweet = 
   `
   <article class="individual-tweet">
-    <div class="avatar-name-account hover-shadow">
+    <div class="avatar-name-account">
       <div class="avatar-name">
         <img id="avatar-image" src="${data.user.avatars}">
         <div>${data.user.name}</div>
       </div>
-      <div class="fade-text"></div>
+      <div class="fade-text">${data.user.handle}</div>
     </div>
-    <div class="tweet-content hover-shadow">
+    <div class="tweet-content">
       <div>${data.content.text}</div>
     </div>
-    <div class="bottom-info hover-shadow">
+    <div class="bottom-info">
       <div>${tweetTime}</div>
       <div>share / like / save</div>
     </div>
@@ -54,7 +73,7 @@ const createTweetElement = function(data) {
   return tweet
 };
 
-const $tweet = createTweetElement(tweetData);
+const $tweet = renderTweets(tweetData);
 
 $(document).ready(function() {
   $('#tweets-container').append($tweet);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,26 +7,9 @@ const inputCounter = function() {
   $('#tweet-text-counter').html(140 - length)
 }
 
-const createHoverShadow = function() {
-  $('.individual-tweet').css('box-shadow', '5px 10px #888888')
-  $('.hover-shadow').css('text-shadow', '0.5px 0.5px #888888')
-  $('#avatar-image').css('filter', 'drop-shadow(1px 1px #888888)')
-  $('.fade-text').html('@rileyjfournier')
-}
-
-const removeHoverShadow = function() {
-  $('.individual-tweet').css('box-shadow', '')
-  $('.hover-shadow').css('text-shadow', '')
-  $('#avatar-image').css('filter', 'drop-shadow(0px 0px #888888)')
-  $('.fade-text').html('')
-}
 
 $(document).ready(function() {
 
   $('#tweet-input').on('keyup', inputCounter)
-
-  $('.individual-tweet').on('mouseover', createHoverShadow)
-
-  $('.individual-tweet').on('mouseout', removeHoverShadow)
 
 })
